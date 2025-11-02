2a34eaac3c3127c9725f3ca364e46ace06d8431d
admin <admin@MacBook-Pro-admin.local>
2025-11-02 20:06:10 +0200

    add vm

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -27,10 +27,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
 }
 
 # Public IP
@@ -39,10 +35,6 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
 }
 
 # Network Interface
@@ -57,12 +49,8 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
 }
+
 # Read SSH key
 data "local_file" "ssh_key" {
   filename = var.ssh_public_key

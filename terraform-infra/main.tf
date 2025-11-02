c0135f4c06bd7a3c6017b9a2fb5c62341f64e4f5
admin <admin@MacBook-Pro-admin.local>
2025-11-02 20:51:42 +0200

    add vm

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
